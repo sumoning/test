a4ff5a24153df0c09914330b42d05fbaebc8273a
xujt <xujt@sugon.com>
2018-09-03 20:11:58 +0800

    commit

---
+++ run_test.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @File  : run_test.py
+# @Author: Xujt
+# @Date  : 2018/9/3
+# @Desc  :
+
+import time
+
+if __name__ == '__main__':
+    for i in range(10):
+        print 'jenkins'
+        time.sleep(10)
